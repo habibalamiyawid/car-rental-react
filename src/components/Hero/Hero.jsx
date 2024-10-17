82726139c0c9600ec3d4f32bc8e7e458f90bb03a
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-10-18 00:08:28 +0600

    fixing hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from 'react';
+import carPng from '../../assets/car.png';
 
 const Hero = () => {
   return (
     <div className="dark:bg-dark dark:text-white duration-300">
        <div className="container min-h-[620px]">
          <div className="grid place-items-center grid-cols-1 sm:grid-cols-2">
-            <div>
+            <div className="order-2 sm:order-2">
                 <img src={carPng} alt="" />
             </div>
-            <div></div>
+            <div className="order-1 sm:order-1">
+                <p className="text-primary text-2xl font-serif">Effortless</p>
+                <h1>Car Rental </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ipsum suscipit ea deleniti provident error distinctio architecto aut!</p>
+            </div>
           
          </div>
        </div>
