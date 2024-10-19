a51f5c8ed0cf47d4f729463ed9ad0b51f449c1cb
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-10-19 23:47:43 +0600

    progressing: Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,15 +4,17 @@ import carPng from '../../assets/car.png';
 const Hero = () => {
   return (
     <div className="dark:bg-dark dark:text-white duration-300">
-       <div className="container min-h-[620px]">
+       <div className="container min-h-[620px] flex">
          <div className="grid place-items-center grid-cols-1 sm:grid-cols-2">
-            <div className="order-2 sm:order-2">
+            <div className="order-1 sm:order-2">
                 <img src={carPng} alt="" />
             </div>
-            <div className="order-1 sm:order-1">
+            <div className="order-2 sm:order-1 space-y-5">
                 <p className="text-primary text-2xl font-serif">Effortless</p>
-                <h1>Car Rental </h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ipsum suscipit ea deleniti provident error distinctio architecto aut!</p>
+                <h1 className="text-5xl lg:text-7xl font-semibold font-serif">Car Rental </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ipsum suscipit ea deleniti provident error distinctio architecto aut!                  
+                </p>
+                <button className="btn bg-primary text-black px-6 py-2 rounded-md hover:bg-primary/80 duration-300">Get Started</button>
             </div>
           
          </div>
