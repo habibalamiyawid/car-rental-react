f72f94dd0c13981919ec0c657469bc18e8a39468
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-10-21 00:05:19 +0600

    hero section moving car added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 
+import AOS from 'aos';
+import "aos/dist/aos.css";
+
 
 const App = () => {
 
@@ -23,6 +26,21 @@ const App = () => {
     }
     
   }, [theme]);
+
+  //AOS Initialization
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: 'ease-in-out',
+      delay: 100,
+
+    });
+    AOS.refresh();
+  },[]);
+
+
+
   return (
     <div>
       <Navbar theme={theme} setTheme={setTheme}/>
