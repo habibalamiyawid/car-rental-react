c188aed010f07ed937854a7f1c172d37e253b41a
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-10-24 23:17:30 +0600

    about section added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from "react";
+import CarPng from "../../assets/car1.png";
 
 const About = () => {
   return (
-    <div>
+    <div className="dark:bg-dark dark:text-white duration-300 sm:min-h-[600px] sm:grid sm:place-items-center">
         <div className="container">
             <div className="grid grid-cols-1
             sm:grid-cols-2 place-items-center">
@@ -10,9 +11,18 @@ const About = () => {
                     <img src={CarPng} alt="" />
                 </div>
                 <div>
-                    
-                </div>
-                 
+                 <div className="space-y-5 sm:p">
+                    <h1 className="text-3xl sm:text-4xl
+                    font-bold font-serif"
+                    >About Us</h1>
+                    <p>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, possimus. Aspernatur, neque. Fugit, a?
+                    </p>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, earum.
+                    </p>
+                 </div>
+                </div>    
             </div>
         </div>
     </div>
