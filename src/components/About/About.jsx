c87902f07a8d3000e172b802b130efe7ec2f787c
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-10-26 00:46:58 +0600

    about Us complete

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,32 +3,34 @@ import CarPng from "../../assets/car1.png";
 
 const About = () => {
   return (
-    <div className="dark:bg-dark dark:text-white duration-300 sm:min-h-[600px] sm:grid sm:place-items-center">
+    <div className="dark:bg-dark bg-slate-100 dark:text-white duration-300 sm:min-h-[600px] sm:grid sm:place-items-center">
         <div className="container">
             <div className="grid grid-cols-1
             sm:grid-cols-2 place-items-center">
-                <div>
+                <div
+                data-aos="slide-right"
+                data-aos-duration ="1500"             
+                >
                     <img src={CarPng} alt="" 
                     className="sm:scale-105
-                    sm:-translate-x-11 max-h-[300px] drop-shadow-[]"/>
+                    sm:-translate-x-11 max-h-[300px] drop-shadow-[2px_10px_6px_rgba(0,0,0,0.50)]"/>
                 </div>
                 <div>
-                 <div className="space-y-5 sm:p">
-                    <h1 className="text-3xl sm:text-4xl
+                 <div className="space-y-5 sm:p-16 pb-6">
+                    <h1 data-aos="fade-up"
+                    className="text-3xl sm:text-4xl
                     font-bold font-serif"
                     >About Us</h1>
-                    <p>
+                    <p data-aos="fade-up">
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, possimus. Aspernatur, neque. Fugit, a?
                     </p>
-                    <p>
+                    <p data-aos="fade-up">
                       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, earum.
                     </p>
-                    <button
-                        data-aos="fade-up"
-                        data-aos-duration="1500" 
-                
-                          className="btn bg-primary text-black px-6 py-2 rounded-md hover:bg-primary/80 duration-300">Get Started
-                </button>
+                    <button data-aos="fade-up"
+                    className="button-outline">
+                      Get Started
+                    </button>
                  </div>
                 </div>    
             </div>
