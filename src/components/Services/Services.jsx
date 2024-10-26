2a9805606641152da31b943d503e4a8ea25d46e1
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-10-27 00:14:36 +0600

    service completed

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React from 'react';
+import{FaCameraRetro} from "react-icons/fa";
+import { GiNotebook } from "react-icons/gi";
+import { SlNote } from "react-icons/sl";
 
+const skillsData = [
+  
+    {
+      name: "Best Price",
+      icon:(
+        <FaCameraRetro className="text-5xl
+        text-primary group-hover:text-black
+        duration-300"/>
+      ),
+      link:"#",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia placeat sint doloremque harum porro!",
+      aosDelay: "0",
+    },
+    {
+      name: "Fast and Safe",
+      icon:(
+        <GiNotebook className="text-5xl
+        text-primary group-hover:text-black
+        duration-300"/>
+      ),
+      link:"#",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia placeat sint doloremque harum porro!",
+      aosDelay: "500",
+    },
+    {
+      name: "Experiend Driver",
+      icon:(
+        <SlNote className="text-5xl
+        text-primary group-hover:text-black
+        duration-500"/>
+      ),
+      link:"#",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia placeat sint doloremque harum porro!",
+      aosDelay: "1000",
+    }
+  
+];
 const Services = () => {
   return (
-    <div className="">
+    <div className="py-14 dark:bg-black
+    dark:text-white sm:min-h[600px] sm:grid
+    sm:place-items-center">
       <div className="container">
         <div className='pb-12'>
           <h1 className="text-3xl font-semibold
@@ -10,6 +52,27 @@ const Services = () => {
             Why Choose Us
           </h1>
         </div>
+        <div className="grid grid-cols-1
+        sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {
+            skillsData.map((skill) =>(
+             <div
+             key={skill.name}
+             data-aos="fade-up"
+             data-aos-delay={skill.aosDelay}
+             className="card text-center group-space-y-3
+             sm:space-y-6 p-4 sm:py-16 bg-dark hover:bg-primary text-white duration-300 text-white hover:text-black rounded-lg"
+             >
+              <div className="grid place-items-center text-black ">{skill.icon}</div>
+              <h1>{skill.name}</h1>
+              <p>{skill.description}</p>
+              <a href={skill.link}>Learn More</a>
+            </div>
+            ))
+          }
+        </div>
+
+
       </div>
     </div>
   )
