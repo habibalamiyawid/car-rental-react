fe891f3d0a657a73ebed0b4af9498db8cf7c7e99
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-10-30 23:11:48 +0600

    carlisted done

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,5 +1,29 @@
-import React from 'react'
+import React from "react";
+import car1 from "../../assets/car1.png";
+import car2 from "../../assets/car5.png";
+import car3 from "../../assets/car6.png";
 
+
+const CarListData =[
+  {
+    name:"BMW UX",
+    price: 100,
+    image: car1,
+    aosDelay: "0",
+  },
+  {
+    name:"KIA UX",
+    price: 140,
+    image: car2,
+    aosDelay: "500",
+  },
+  {
+    name:"BMW UX",
+    price: 100,
+    image: car3,
+    aosDelay: "1000",
+  },
+]
 const CarList = () => {
   return (
     <div className="pb-24 pt-12 bg-white dark:bg-dark
@@ -16,10 +40,42 @@ const CarList = () => {
            </p>
            {/* Car listing cards */}
            <div>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16'>
+            <div 
+            className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16'>
                 {
-                    CarList.map(()=>{
-                        
+                  CarListData.map((data)=>{
+                    return(
+                    <div 
+                    data-aos="fade-up"
+                    data-aos-delay={data.aosDelay}
+                    className="space-y-3 border-2
+                    border-grey-300 hover:border-primary
+                    p-3 rounded-xl relative group"
+                    > 
+
+                      <div className="w-full h-[120px]">
+                        <img
+                         className="w-full h-[120px] object-contain sm:translate-x-8
+                         group-hover:translate-x-16
+                         duration-700"
+                         src={data.image} alt=""/>
+                      </div> 
+                      <div className="space-y-2">
+                        <h1 className="text-primary font-semibold">
+                          {data.name}
+                        </h1>
+                      <div className="flex 
+                      justify-between items-center 
+                      text-xl font-semibold">
+                          <p>${data.price}/Day</p>
+                          <a href="#">Details</a>
+                      </div>
+                      </div>
+                        <p className="text-xl font-semibold absolute top-0 left-3">12 Km</p>
+
+
+                    </div>
+                    );
 
 
 
@@ -28,6 +84,15 @@ const CarList = () => {
 
             </div>
            </div>
+            {/* end of car listing cards */}
+             <div className="grid place-content-center
+             mt-8">
+              <button data-aos="fade-up"
+              className="button-outline">
+                 Get Started
+
+              </button>
+             </div>
         </div>
     </div>
   )
