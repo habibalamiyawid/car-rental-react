7518063c576915fc0a28307032308ed8287a7ed9
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-11-01 00:16:07 +0600

    progress: textimonial

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -10,7 +10,19 @@ const Testimonial = () => {
             <p className="text-3xl font-semibold
             text-center sm:text-4xl font-serif">
              What Our Client Say About Us</p>
-             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam aspernatur quaerat vitae nesciunt in.</p>
+            <p data-aos="fade-up"
+            className="text-center sm:px-44">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam aspernatur quaerat vitae nesciunt in.{" "}
+            </p>
+          </div>
+          {/* card section */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 text-black dark:text-white">
+            {
+                testimonialData.map((data) =>{
+
+                })
+            }
+
           </div>
       </div>
     </div>
