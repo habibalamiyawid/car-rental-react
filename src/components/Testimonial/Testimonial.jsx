53610cd54c0c9bef2500c2adda73ae435e4ffdcf
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-11-01 23:48:02 +0600

    testimonial complete

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -39,10 +39,14 @@ const Testimonial = () => {
         md:grid-cols-3 gap-8 text-black dark:text-white">
           {testimonialData.map((data) => {
             return (
-              <div key={data.name}
+              
+              <div 
+              data-aos="fade-up"
+              data-aos-delay={data.aosDelay}
+              key={data.name}
               className="card text-center group space-y-3
               p-4 sm:space-y-6 p-4 bg-gray-100 dark:bg-white/20
-              sm:py-12 duration-300">
+              sm:py-12 duration-300 rounded-lg">
                 <div className="grid place-items-center">
                   <img src="https://picsum.photos/200" 
                   alt= "" 
