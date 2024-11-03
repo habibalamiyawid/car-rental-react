e4663afc2ddd625cb3c50cccae0f0f527038a616
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-11-03 11:05:53 +0600

    jpeg config added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Adjusted path to resolve to the src directory
+    },
+  },
+});
