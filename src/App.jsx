d7b8f29b6a55f5d7ccfce502e17333582a5095a0
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-11-03 11:39:44 +0600

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./components/About/About";
 import Services from "./components/Services/Services";
 import CarList from "./components/CarList/CarList";
 import AppStoreBanner from "./components/AppStoreBanner/AppStoreBanner";
+import Contact from "./components/Contact/Contact";
 
 import AOS from 'aos';
 import "aos/dist/aos.css";
@@ -56,7 +57,7 @@ const App = () => {
       <CarList/>
       <Testimonial/>
       <AppStoreBanner/>
-      <Contact />
+      <Contact/>
     </div>
   )
 }
